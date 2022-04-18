522e666842880463467df08be5978f83f6d06124
Vithushan Logeswaran <2018csc042@univ.jfn.ac.lk>
2022-04-18 11:50:11 +0530

    basic function added

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,3 +1,9 @@
+import "@nomiclabs/hardhat-waffle";
+import "@nomiclabs/hardhat-ethers";
+
+
+
+
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
---
+++ test/test.ts
@@ -0,0 +1,9 @@
+import "@nomiclabs/hardhat-ethers";
+import { ethers } from "hardhat";
+import { expect } from "chai";
+
+describe("hello world", function(){
+    it("should say hi", async function() {
+        
+    })
+})
